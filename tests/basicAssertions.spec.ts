2f60f948090e891325c5bf6d275cf159f7deb6d2
komalgc <komalgc17@gmail.com>
2026-06-02 16:24:18 -0600

    Practicetest

---
+++ tests/basicAssertions.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Login and Validate', {tag: "@links"},  async ({ page }) => {
+
+    await page.goto('https://practicesoftwaretesting.com/auth/login');
+    await page.getByPlaceholder('Your email').fill('customer@practicesoftwaretesting.com');
+    await page.getByPlaceholder('Your password').fill('welcome01');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByTestId('nav-home').click();
+
+    await expect(page.locator('.col-md-9').getByRole('link')).toHaveCount(9);
+    const productscount = await page.locator('.col-md-9').getByRole('link').count();
+
+    const products = await  page.locator('.col-md-9').getByRole('link');
+
+    for (let i =0; i<productscount; i++){
+        const productcard = products.nth(i);
+        const productname = await productcard.getByTestId('product-name').innerText();
+
+        if (productname.trim() === 'Thor Hammer'){
+            await productcard.click();
+            await expect(page.getByRole('heading', { name: 'Thor Hammer' })).toBeVisible();
+            break;
+
+        }
+
+
+
+    }
+});
---
+++ tests/basicTest.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', {tag: "@first"},async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Practice Software Testing/);
+});
+
+test('Sign In', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login');
+
+  // Click the get started link.
+  await page.getByPlaceholder('Your email').fill('customer@practicesoftwaretesting.com');
+  await page.getByPlaceholder('Your password').fill('welcome01');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'My account' })).toBeVisible();
+});
